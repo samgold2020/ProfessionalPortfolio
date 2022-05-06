c95245ff7a9be6618dfee46face4d48f2a17a5b1
Sam Goldstein <sgoldstein312@gmail.com>
2022-05-05 21:49:08 -0500

    Update styles, self closing tag

--- src/components/skills.js
+++ src/components/skills.js
@@ -3,14 +3,14 @@ import data from "../yourdata"
 
 const Skills = () => {
   return (
-    <div className="section">
+    <div className="section" id="skills">
       <div className="container">
         <div className="skills-container">
           <h1>Skills</h1>
           <div className="skills-grid">
             {data.skills.map((skill, index) => (
               <div className="skill" key={index}>
-                <img src={skill.img} alt="css"></img>
+                <img src={skill.img} alt="css" />
               </div>
             ))}
           </div>
